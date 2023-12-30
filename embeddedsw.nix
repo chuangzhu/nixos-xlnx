90b867de95ff72a6f093728bfa17c07a0bc24159
Chuang Zhu <git@chuang.cz>
2023-12-30 10:35:03 +0800

    fsbl

--- embeddedsw.nix
+++ embeddedsw.nix
@@ -34,22 +34,44 @@ in
       install -Dm555 lib/sw_apps/zynqmp_pmufw/src/executable.elf -T $out/pmufw.elf
       runHook postInstall
     '';
-
-    dontShrink = true;
+    dontStrip = true;
 
     meta = with lib; {
       description = "Zynq MPSoC Platform Management Unit firmware";
+      homepage = "https://xilinx-wiki.atlassian.net/wiki/spaces/A/pages/18841724/PMU+Firmware";
       license = licenses.mit;
       platforms = [ "microblazeel-none" ];
+      maintainer = with maintainers; [ chuangzhu ];
     };
   });
 
   zynqmp-fsbl = stdenv.mkDerivation (finalAttrs: {
     pname = "zynqmp-fsbl";
+    inherit version src;
+
+    postPatch = ''
+      patchShebangs lib/sw_apps/zynqmp_fsbl/misc/copy_bsp.sh
+    '';
+    makeFlags = [
+      "-C" "lib/sw_apps/zynqmp_fsbl/src"
+      "CC:=$(CC)" "COMPILER=$(CC)"
+      "AR:=$(AR)" "ARCHIVER=$(AR)"
+      "CROSS_COMP=${stdenv.targetPlatform.config}"
+    ];
+
+    installPhase = ''
+      runHook preInstall
+      install -Dm555 lib/sw_apps/zynqmp_fsbl/src/fsbl.elf -t $out/
+      runHook postInstall
+    '';
+    dontStrip = true;
 
     meta = with lib; {
+      description = "Zynq MPSoC First Stage Boot Loader";
+      homepage = "https://xilinx-wiki.atlassian.net/wiki/spaces/A/pages/18842019/FSBL";
       license = licenses.mit;
       platforms = [ "aarch64-none" ];
+      maintainer = with maintainers; [ chuangzhu ];
     };
   });
 }
