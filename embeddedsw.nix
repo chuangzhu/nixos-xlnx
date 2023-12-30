6d8c1dd86fbf0c7f457e2485a3a72850d7467989
Chuang Zhu <git@chuang.cz>
2023-12-30 09:59:49 +0800

    pmufw

--- embeddedsw.nix
+++ embeddedsw.nix
@@ -17,7 +17,25 @@ in
   zynqmp-pmufw = stdenv.mkDerivation (finalAttrs: {
     pname = "zynqmp-pmufw";
     inherit version src;
-    sourceRoot = "source/lib/sw_apps/zynqmp_pmufw/src";
+
+    postPatch = ''
+      patchShebangs lib/sw_apps/zynqmp_pmufw/misc/copy_bsp.sh
+      substituteInPlace lib/sw_apps/zynqmp_pmufw/misc/xparameters.h \
+        --replace "XPAR_MICROBLAZE_USE_BARREL 1" "XPAR_MICROBLAZE_USE_BARREL 0"
+    '';
+    makeFlags = [
+      "-C" "lib/sw_apps/zynqmp_pmufw/src"
+      "CC:=$(CC)" "COMPILER=$(CC)"
+      "AR:=$(AR)" "ARCHIVER=$(AR)"
+    ];
+
+    installPhase = ''
+      runHook preInstall
+      install -Dm555 lib/sw_apps/zynqmp_pmufw/src/executable.elf -T $out/pmufw.elf
+      runHook postInstall
+    '';
+
+    dontShrink = true;
 
     meta = with lib; {
       description = "Zynq MPSoC Platform Management Unit firmware";
