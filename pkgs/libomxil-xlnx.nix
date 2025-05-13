chuangzhu/nixos-xlnx

commit 36d6d3c995427ea783132054bf778860928b6a71

2023.2 -> 2024.1

pkgs/libomxil-xlnx.nix
@@ -6,13 +6,13 @@
 
 stdenv.mkDerivation rec {
   pname = "libomxil-xlnx";
-  version = "2023.2";
+  version = "2024.1";
 
   src = fetchFromGitHub {
     owner = "Xilinx";
     repo = "vcu-omx-il";
     rev = "xilinx_v${version}";
-    hash = "sha256-IlYO2NLxaj8pLQwphNs27VL06KUo2ouWzrGmm0MIseM=";
+    hash = "sha256-mqD0F2V/kh5NzpkniHcCcclJ7Suzgn67+Kf4UAQAK7E=";
   };
 
   postPatch = ''

pkgs/libvcu-xlnx.nix
@@ -5,13 +5,13 @@
 
 stdenv.mkDerivation rec {
   pname = "libvcu-xlnx";
-  version = "2023.2";
+  version = "2024.1";
 
   src = fetchFromGitHub {
     owner = "Xilinx";
     repo = "vcu-ctrl-sw";
     rev = "xilinx_v${version}";
-    hash = "sha256-vayjjI7e3QNzg07l/FQNmx0BkZ0y6qENzDZ1a47t3iE=";
+    hash = "sha256-vLbzJktS7RFvPCpkNG07puSUxvd0yoe2R2rsltp77sE=";
   };
 
   installTargets = [ "install_headers" ];

boot-bin.nix
@@ -75,7 +75,7 @@
             }
           '';
         }.${config.hardware.zynq.platform};
-      in pkgs.runCommand "BOOT.BIN" { nativeBuildInputs = [ pkgs.xilinx-bootgen_2023_2 ]; } ''
+      in pkgs.runCommand "BOOT.BIN" { nativeBuildInputs = [ pkgs.xilinx-bootgen_2024_1 ]; } ''
         bootgen -image ${pkgs.writeText "bootgen.bif" bif} -arch ${config.hardware.zynq.platform} -w -o $out
       '';
       description = lib.mdDoc ''

overlay.nix
@@ -82,13 +82,13 @@ final: prev: {
     });
   };
 
-  xilinx-bootgen_2023_2 = prev.xilinx-bootgen.overrideAttrs rec {
-    version = "xilinx_v2023.2";
+  xilinx-bootgen_2024_1 = prev.xilinx-bootgen.overrideAttrs rec {
+    version = "xilinx_v2024.1";
     src = prev.fetchFromGitHub {
       owner = "Xilinx";
       repo = "bootgen";
       rev = version;
-      hash = "sha256-YRaq36N6uBHyjuHQ5hCO35Y+y818NuSjg/js181iItA=";
+      hash = "sha256-/gNAqjwfaD2NWxs2536XGv8g2IyRcQRHzgLcnCr4a34=";
     };
   };
   python-lopper = prev.python3Packages.callPackage ./pkgs/lopper.nix { };

pkgs/arm-trusted-firmware-xlnx.nix
@@ -5,12 +5,12 @@
 }:
 
 buildArmTrustedFirmware rec {
-  version = "2.8.0";
+  version = "2.10";
   src = fetchFromGitHub {
     owner = "Xilinx";
     repo = "arm-trusted-firmware";
-    rev = "xilinx-v2023.2";
-    hash = "sha256-RvdBsskiSgquwnDf0g0dU8P6v4QxK4OqhtkF5K7lfyI=";
+    rev = "xlnx_rebase_v2.10_2024.1";
+    hash = "sha256-XEFHS2hZWdJEB7b0Zdci/PtNc7csn+zQWljiG9Tx0mM=";
   };
   extraMakeFlags = [ "bl31" ];
   platform = "zynqmp";

pkgs/dp-modules.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   name = "xlnx-dp-modules-${kernel.version}-${finalAttrs.version}";
-  version = "2023.2";
+  version = "2024.1";
 
   src = fetchFromGitHub {
     owner = "Xilinx";
     repo = "dp-modules";
     rev = "xilinx_v${finalAttrs.version}";
-    hash = "sha256-lxo2HCk1vwLmy/Y3NRPohLzj6OZ3p0IlbjWK0M7pkkI=";
+    hash = "sha256-uWy83c7W0oebvqkhmwkXejFredM4ACO0Cb0jqfeA25Q=";
   };
 
   nativeBuildInputs = kernel.moduleBuildDependencies ++ [ ];

pkgs/embeddedsw.nix
@@ -8,12 +8,12 @@
 }:
 
 let
-  version = "2023.2";
+  version = "2024.1";
   src = fetchFromGitHub {
     owner = "Xilinx";
     repo = "embeddedsw";
     rev = "xilinx_v${version}";
-    hash = "sha256-koBo9gmkWBqA6PaN5eNsnkCQRaeDWeHm/qBN8/ARW+E=";
+    hash = "sha256-vh7tdHNd3miDZplTiRP8UWhQ/HLrjMcbQXCJjTO4p9o=";
   };
   python = python3.withPackages (p: [ python-lopper p.pyyaml ]);
 in

pkgs/hdmi-modules.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   name = "xlnx-hdmi-modules-${kernel.version}-${finalAttrs.version}";
-  version = "2023.2";
+  version = "2024.1";
 
   src = fetchFromGitHub {
     owner = "Xilinx";
     repo = "hdmi-modules";
     rev = "xilinx_v${finalAttrs.version}";
-    hash = "sha256-49pdwXKJ8Y7mk5+iQZGQpi2rC/ffYIk/RXHGoXS9vko=";
+    hash = "sha256-YZENLz7C54yVeqbIhMnEfnS1g0lTzu8Rr1GaKH7a0Z4=";
   };
 
   nativeBuildInputs = kernel.moduleBuildDependencies ++ [ ];

commit eb559498c6badeb2caa479d28c02a0ea2a6ee8a1

fix libomxil not finding libvcu
Bug introduced in 36d6d3c995427ea783132054bf778860928b6a71

[omx_core.cpp:58]	[OMX_Init]	liballegro_encode.so.0: cannot open shared object file: No such file or directory
[omx_core.cpp:58]	[OMX_Init]	liballegro_encode.so.0: cannot open shared object file: No such file or directory
[omx_core.cpp:58]	[OMX_Init]	liballegro_encode.so.0: cannot open shared object file: No such file or directory
[omx_core.cpp:58]	[OMX_Init]	liballegro_encode.so.0: cannot open shared object file: No such file or directory
[omx_core.cpp:58]	[OMX_Init]	liballegro_decode.so.0: cannot open shared object file: No such file or directory
[omx_core.cpp:58]	[OMX_Init]	liballegro_decode.so.0: cannot open shared object file: No such file or directory
[omx_core.cpp:58]	[OMX_Init]	liballegro_decode.so.0: cannot open shared object file: No such file or directory
[omx_core.cpp:58]	[OMX_Init]	liballegro_decode.so.0: cannot open shared object file: No such file or directory
[omx_core.cpp:65]	[OMX_Init]	No library found! Did you set OMX_ALLEGRO_PATH?

pkgs/libomxil-xlnx.nix
@@ -29,7 +29,7 @@ stdenv.mkDerivation rec {
     for f in $out/lib/*.so; do ln -s "$f" "$f".1; done
   '' + lib.optionalString (lib.versionAtLeast version "2023.2") ''
     install -Dm555 bin/omx_encoder.exe -T $out/bin/omx_encoder
-    install -Dm555 bin/omx_decoder.exe -T $out/bin/omx_encoder
+    install -Dm555 bin/omx_decoder.exe -T $out/bin/omx_decoder
   '' + lib.optionalString (lib.versionOlder version "2023.2") ''
     install -Dm555 bin/omx_{en,de}coder -t $out/bin/
   '' + ''
@@ -39,7 +39,11 @@ stdenv.mkDerivation rec {
   meta = with lib; {
     description = "OpenMAX Integration Layer implementation for Xilinx Zynq UltraScale+ VCU";
     homepage = "https://xilinx-wiki.atlassian.net/wiki/spaces/A/pages/18842546/Xilinx+Zynq+UltraScale+MPSoC+Video+Codec+Unit";
-    license = licenses.unfree; # Based on X11 license, but with two extra terms
+    license =
+      if lib.versionOlder version "2023.1" then
+        licenses.unfree # Based on X11 license, but with two extra terms
+      else
+        licenses.mit;
     sourceProvenance = with sourceTypes; [ fromSource ];
     platforms = platforms.linux;
     maintainers = with maintainers; [ chuangzhu ];

pkgs/libvcu-xlnx.nix
@@ -22,8 +22,8 @@ stdenv.mkDerivation rec {
   ];
 
   postInstall = ''
-    for f in $out/lib/*.so; do ln -s "$f" "$f".0; done
     install -Dm755 bin/liballegro_{en,de}code.so -t $out/lib/
+    for f in $out/lib/*.so; do ln -s "$f" "$f".0; done
   '' + lib.optionalString (lib.versionOlder version "2023.2") ''
     install -Dm755 bin/ctrlsw_{en,de}coder -t $out/bin/
   '';
@@ -31,7 +31,11 @@ stdenv.mkDerivation rec {
   meta = with lib; {
     description = "Xilinx Zynq UltraScale+ VCU control software";
     homepage = "https://xilinx-wiki.atlassian.net/wiki/spaces/A/pages/18842546/Xilinx+Zynq+UltraScale+MPSoC+Video+Codec+Unit";
-    license = licenses.unfree; # Based on X11 license, but with two extra terms
+    license =
+      if lib.versionOlder version "2023.1" then
+        licenses.unfree # Based on X11 license, but with two extra terms
+      else
+        licenses.mit;
     sourceProvenance = with sourceTypes; [ fromSource ];
     platforms = platforms.linux;
     maintainers = with maintainers; [ chuangzhu ];